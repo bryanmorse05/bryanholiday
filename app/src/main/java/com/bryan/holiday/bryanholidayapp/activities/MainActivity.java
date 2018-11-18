e3983607803dc24241713fdf2bb5ea87d1f6d0cf
Bryan <bryanmorse05@gmail.com>
2018-11-17 17:20:48 -0800

    Main screen, audio some in.

--- app/src/main/java/com/bryan/holiday/bryanholidayapp/activities/MainActivity.java
+++ app/src/main/java/com/bryan/holiday/bryanholidayapp/activities/MainActivity.java
@@ -1,16 +1,20 @@
 package com.bryan.holiday.bryanholidayapp.activities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.bryan.holiday.bryanholidayapp.R;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button videoButton;
+    Button videoButton, yuleLogButton;
+    ImageView titleScreenImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         videoButton = findViewById(R.id.videoButton);
+        yuleLogButton = findViewById(R.id.yuleLogButton);
+        titleScreenImage = findViewById(R.id.titleScreenImage);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.christmas_time_is_here_vocal);
+        mediaPlayer.start();
 
         videoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        yuleLogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), YuleLog_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Glide.with(titleScreenImage).load(R.drawable.original).into(titleScreenImage);
 
     }
 }
---
+++ app/src/main/java/com/bryan/holiday/bryanholidayapp/activities/YuleLog_Activity.java
@@ -0,0 +1,33 @@
+package com.bryan.holiday.bryanholidayapp.activities;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bryan.holiday.bryanholidayapp.R;
+import com.bumptech.glide.Glide;
+
+public class YuleLog_Activity extends AppCompatActivity {
+
+    ImageView yuleLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yule_log_);
+
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        getSupportActionBar().hide();
+
+        yuleLog = findViewById(R.id.yuleLogImage);
+
+        Glide.with(yuleLog).load(R.drawable.yule_log).into(yuleLog);
+    }
+}
