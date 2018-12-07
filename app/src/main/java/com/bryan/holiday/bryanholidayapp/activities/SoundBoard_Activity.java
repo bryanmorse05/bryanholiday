588d5206e9d787ff6fefb58ae50d795e7e0247b2
bryanmorse <bryanmorse05@gmail.com>
2018-12-06 17:51:34 -0800

    Changed the title for one of the mp3s.

--- app/src/main/java/com/bryan/holiday/bryanholidayapp/activities/SoundBoard_Activity.java
+++ app/src/main/java/com/bryan/holiday/bryanholidayapp/activities/SoundBoard_Activity.java
@@ -1,19 +1,58 @@
 package com.bryan.holiday.bryanholidayapp.activities;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.bryan.holiday.bryanholidayapp.R;
 
 public class SoundBoard_Activity extends AppCompatActivity {
+
+    Button button1, button2;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_board_);
 
+        button1 = findViewById(R.id.soundButton1);
+        button2 = findViewById(R.id.soundButton2);
+
+        mediaPlayer = new MediaPlayer();
+
         //Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Sound Board");
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    mediaPlayer.release();
+                }
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bumpuses);
+                mediaPlayer.start();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                }
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.commercial);
+                mediaPlayer.start();
+            }
+        });
+
+
     }
 }
